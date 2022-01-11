9d7347a05c6775c9754186546d566f86509e410d
iJorgen <52280788+iJorgen@users.noreply.github.com>
2022-01-11 09:21:33 +0100

    Update firewall.sh

--- firewall.sh
+++ firewall.sh
@@ -55,7 +55,6 @@ blocklist_set="		<BinaryDefense>			https://iplists.firehol.org/files/bds_atif.ip
 					<CIArmy>				https://iplists.firehol.org/files/ciarmy.ipset  {1}
 					<CiscoTalos>			https://www.talosintelligence.com/documents/ip-blacklist  {4}
 					<Cruzit_Webattacks>		https://iplists.firehol.org/files/cruzit_web_attacks.ipset  {2}
-					<Cybercrime>			https://iplists.firehol.org/files/cybercrime.ipset  {4}
 					<DanMe_TOR>				https://iplists.firehol.org/files/dm_tor.ipset  {1}
 					<Darklist>				https://iplists.firehol.org/files/darklist_de.netset  {4}
 					<Dshield_7d>			https://iplists.firehol.org/files/dshield_7d.netset  {1}
