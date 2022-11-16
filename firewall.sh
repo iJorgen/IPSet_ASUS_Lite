7346d81c095b7ef9867a481755e53f6eeb16d7d3
iJorgen <52280788+iJorgen@users.noreply.github.com>
2022-11-16 16:57:25 +0100

    Update firewall.sh

--- firewall.sh
+++ firewall.sh
@@ -57,9 +57,10 @@ loginvalid="disabled"	# enabled | disabled
 blocklist_set="		<BinaryDefense>			https://iplists.firehol.org/files/bds_atif.ipset  {2}
 					<CIArmy>				https://iplists.firehol.org/files/ciarmy.ipset  {2}
 					<CiscoTalos>			https://www.talosintelligence.com/documents/ip-blacklist  {4}
+					<DanMe_TOR>				https://iplists.firehol.org/files/dm_tor.ipset  {4}
 					<Dshield_7d>			https://iplists.firehol.org/files/dshield_7d.netset  {2}
 					<GreenSnow>				https://iplists.firehol.org/files/greensnow.ipset  {2}
-					<IPSum_Level2>			https://raw.githubusercontent.com/stamparm/ipsum/master/levels/2.txt  {4}
+					<IPSum_Level3>			https://raw.githubusercontent.com/stamparm/ipsum/master/levels/3.txt  {4}
 					<Spamhaus_drop>			https://iplists.firehol.org/files/spamhaus_drop.netset  {4}
 					<Spamhaus_edrop>		https://iplists.firehol.org/files/spamhaus_edrop.netset  {4}"
 blocklist_ip=""
