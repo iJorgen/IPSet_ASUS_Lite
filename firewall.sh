c8a2d8cb4dc8dea3afd67a5fbb9c39c1732f8547
iJorgen <52280788+iJorgen@users.noreply.github.com>
2022-11-22 16:19:12 +0100

    Update firewall.sh

--- firewall.sh
+++ firewall.sh
@@ -54,17 +54,16 @@ filtertraffic="all"		# inbound | outbound | all
 logmode="enabled"		# enabled | disabled
 loginvalid="disabled"	# enabled | disabled
 
-blocklist_set="		<BinaryDefense>			https://iplists.firehol.org/files/bds_atif.ipset  {4}
+blocklist_set="		<BinaryDefense>			https://iplists.firehol.org/files/bds_atif.ipset  {2}
 					<CIArmy>				https://iplists.firehol.org/files/ciarmy.ipset  {2}
 					<CiscoTalos>			https://www.talosintelligence.com/documents/ip-blacklist  {4}
 					<DanMe_TOR>				https://iplists.firehol.org/files/dm_tor.ipset  {2}
 					<Dshield_7d>			https://iplists.firehol.org/files/dshield_7d.netset  {2}
 					<GreenSnow>				https://iplists.firehol.org/files/greensnow.ipset  {2}
 					<IPSum_Level2>			https://raw.githubusercontent.com/stamparm/ipsum/master/levels/2.txt  {4}
-					<Socks_Proxy_1d>		https://iplists.firehol.org/files/socks_proxy_1d.ipset  {2}
 					<Spamhaus_drop>			https://iplists.firehol.org/files/spamhaus_drop.netset  {4}
 					<Spamhaus_edrop>		https://iplists.firehol.org/files/spamhaus_edrop.netset  {4}
-					<SSL_Proxy_1d>			https://iplists.firehol.org/files/sslproxies_1d.ipset  {2}"
+					<StrongIPs>				https://iplists.firehol.org/files/blocklist_de_strongips.ipset {4}"
 blocklist_ip=""
 blocklist_domain=""
 
