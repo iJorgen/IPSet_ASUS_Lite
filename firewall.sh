fd4d4693dd1f4a6230b8eb6ff04aa09b7bb8ccdd
iJorgen <52280788+iJorgen@users.noreply.github.com>
2022-11-22 21:18:49 +0100

    Update firewall.sh

--- firewall.sh
+++ firewall.sh
@@ -62,8 +62,7 @@ blocklist_set="		<BinaryDefense>			https://iplists.firehol.org/files/bds_atif.ip
 					<GreenSnow>				https://iplists.firehol.org/files/greensnow.ipset  {2}
 					<IPSum_Level2>			https://raw.githubusercontent.com/stamparm/ipsum/master/levels/2.txt  {4}
 					<Spamhaus_drop>			https://iplists.firehol.org/files/spamhaus_drop.netset  {4}
-					<Spamhaus_edrop>		https://iplists.firehol.org/files/spamhaus_edrop.netset  {4}
-					<StrongIPs>				https://iplists.firehol.org/files/blocklist_de_strongips.ipset {4}"
+					<Spamhaus_edrop>		https://iplists.firehol.org/files/spamhaus_edrop.netset  {4}"
 blocklist_ip=""
 blocklist_domain=""
 
