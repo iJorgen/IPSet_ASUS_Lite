8e25c30e812c78014ad10bb0e507c6bc783400ce
iJorgen <52280788+iJorgen@users.noreply.github.com>
2022-11-27 20:42:53 +0100

    Update firewall.sh

--- firewall.sh
+++ firewall.sh
@@ -58,10 +58,11 @@ blocklist_set="		<BinaryDefense>			https://iplists.firehol.org/files/bds_atif.ip
 					<CIArmy>				https://iplists.firehol.org/files/ciarmy.ipset  {4}
 					<CiscoTalos>			https://www.talosintelligence.com/documents/ip-blacklist  {6}
 					<DanMe_TOR>				https://iplists.firehol.org/files/dm_tor.ipset  {2}
-					<Dshield_1d>			https://iplists.firehol.org/files/dshield_1d.netset  {2}
+					<Dshield_7d>			https://iplists.firehol.org/files/dshield_7d.netset  {2}
 					<GreenSnow>				https://iplists.firehol.org/files/greensnow.ipset  {2}
 					<IPSum_Level2>			https://raw.githubusercontent.com/stamparm/ipsum/master/levels/2.txt  {6}
-					<Socks_Proxy_1d>		https://iplists.firehol.org/files/socks_proxy_1d.ipset  {4}
+					<Socks_Proxy_7d>		https://iplists.firehol.org/files/socks_proxy_7d.ipset  {2}
+					<SSL_Proxy_7d>			https://iplists.firehol.org/files/sslproxies_7d.ipset  {2}
 					<Spamhaus_drop>			https://iplists.firehol.org/files/spamhaus_drop.netset  {8}
 					<Spamhaus_edrop>		https://iplists.firehol.org/files/spamhaus_edrop.netset  {8}
 					<ThreatView_OSINT>		https://threatview.io/Downloads/Experimental-IOC-Tweets.txt  {8}
