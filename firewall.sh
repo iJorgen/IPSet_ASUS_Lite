a013548a101b037633c6e607976ff8efa3d31e1d
iJorgen <52280788+iJorgen@users.noreply.github.com>
2023-04-18 10:36:25 +0200

    Update firewall.sh

--- firewall.sh
+++ firewall.sh
@@ -56,12 +56,16 @@ loginvalid="enabled"	# enabled | disabled
 
 blocklist_set="		<AbuseIPDB>				https://raw.githubusercontent.com/borestad/blocklist-ip/main/abuseipdb-s100.ipv4  {1}
 					<BinaryDefense>         https://iplists.firehol.org/files/bds_atif.ipset  {2}
+                    <China>					https://www.ipdeny.com/ipblocks/data/aggregated/cn-aggregated.zone  {4}
 					<CIArmy>				https://iplists.firehol.org/files/ciarmy.ipset  {1}
                     <CiscoTalos>            https://www.talosintelligence.com/documents/ip-blacklist  {2}
                     <CyberCrime>			https://iplists.firehol.org/files/cybercrime.ipset  {1}
                     <DanMe_TOR>				https://iplists.firehol.org/files/dm_tor.ipset  {1}
                     <GreenSnow>             https://iplists.firehol.org/files/greensnow.ipset  {1}
                     <IPSum_Level2>          https://raw.githubusercontent.com/stamparm/ipsum/master/levels/2.txt  {2}
+                    <Iran>					https://www.ipdeny.com/ipblocks/data/aggregated/ir-aggregated.zone  {4}
+                    <NorthKorea>			https://www.ipdeny.com/ipblocks/data/aggregated/kr-aggregated.zone  {4}
+                    <Russia>				https://www.ipdeny.com/ipblocks/data/aggregated/ru-aggregated.zone  {4}
                     <Spamhaus_drop>         https://iplists.firehol.org/files/spamhaus_drop.netset  {3}
                     <Spamhaus_edrop>        https://iplists.firehol.org/files/spamhaus_edrop.netset  {3}
                     <ThreatView_OSINT>      https://threatview.io/Downloads/Experimental-IOC-Tweets.txt  {2}
