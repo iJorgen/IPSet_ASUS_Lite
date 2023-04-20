cbe37e5d6aa8c0ae58798bb732977887267fdc3a
iJorgen <52280788+iJorgen@users.noreply.github.com>
2023-04-20 21:07:51 +0200

    Update firewall.sh

--- firewall.sh
+++ firewall.sh
@@ -33,14 +33,14 @@ logmode="enabled"		# enabled | disabled
 loginvalid="enabled"	# enabled | disabled
 
 blocklist_set="		<AbuseIPDB>				https://raw.githubusercontent.com/borestad/blocklist-ip/main/abuseipdb-s100.ipv4  {1}
-					<BinaryDefense>			https://www.binarydefense.com/banlist.txt  {2}
+					<BinaryDefense>			https://iplists.firehol.org/files/bds_atif.ipset  {2}
                     <China>					https://www.ipdeny.com/ipblocks/data/aggregated/cn-aggregated.zone  {4}
-					<CIArmy>				https://cinsscore.com/list/ci-badguys.txt  {1}
+					<CIArmy>				https://iplists.firehol.org/files/ciarmy.ipset  {1}
                     <CiscoTalos>			https://www.talosintelligence.com/documents/ip-blacklist  {2}
                     <CyberCrime>			https://iplists.firehol.org/files/cybercrime.ipset  {1}
                     <DanMe_TOR>				https://iplists.firehol.org/files/dm_tor.ipset  {1}
                     <EmergingThreats>		https://rules.emergingthreats.net/fwrules/emerging-Block-IPs.txt  {2}
-                    <GreenSnow>				https://blocklist.greensnow.co/greensnow.txt  {1}
+                    <GreenSnow>				https://iplists.firehol.org/files/greensnow.ipset  {1}
                     <IPSum>					https://raw.githubusercontent.com/stamparm/ipsum/master/levels/2.txt  {2}
                     <Iran>					https://www.ipdeny.com/ipblocks/data/aggregated/ir-aggregated.zone  {4}
                     <NorthKorea>			https://www.ipdeny.com/ipblocks/data/aggregated/kr-aggregated.zone  {4}
