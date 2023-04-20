d800b81594cc94f9bf2177bc637f081359333c98
iJorgen <52280788+iJorgen@users.noreply.github.com>
2023-04-20 21:26:17 +0200

    Update firewall.sh

--- firewall.sh
+++ firewall.sh
@@ -34,7 +34,7 @@ loginvalid="enabled"	# enabled | disabled
 
 blocklist_set="		<AbuseIPDB>				https://raw.githubusercontent.com/borestad/blocklist-ip/main/abuseipdb-s100.ipv4  {1}
 					<BinaryDefense>			https://iplists.firehol.org/files/bds_atif.ipset  {2}
-                    <China>					https://www.ipdeny.com/ipblocks/data/aggregated/cn-aggregated.zone  {4}
+                    <China>					https://www.ipdeny.com/ipblocks/data/aggregated/cn-aggregated.zone  {6}
 					<CIArmy>				https://iplists.firehol.org/files/ciarmy.ipset  {1}
                     <CiscoTalos>			https://www.talosintelligence.com/documents/ip-blacklist  {2}
                     <CyberCrime>			https://iplists.firehol.org/files/cybercrime.ipset  {1}
@@ -42,13 +42,16 @@ blocklist_set="		<AbuseIPDB>				https://raw.githubusercontent.com/borestad/block
                     <EmergingThreats>		https://rules.emergingthreats.net/fwrules/emerging-Block-IPs.txt  {2}
                     <GreenSnow>				https://iplists.firehol.org/files/greensnow.ipset  {1}
                     <IPSum>					https://raw.githubusercontent.com/stamparm/ipsum/master/levels/2.txt  {2}
-                    <Iran>					https://www.ipdeny.com/ipblocks/data/aggregated/ir-aggregated.zone  {4}
-                    <NorthKorea>			https://www.ipdeny.com/ipblocks/data/aggregated/kr-aggregated.zone  {4}
-                    <Russia>				https://www.ipdeny.com/ipblocks/data/aggregated/ru-aggregated.zone  {4}
+                    <Iran>					https://www.ipdeny.com/ipblocks/data/aggregated/ir-aggregated.zone  {6}
+                    <Israel>				https://www.ipdeny.com/ipblocks/data/aggregated/il-aggregated.zone  {6}
+                    <NorthKorea>			https://www.ipdeny.com/ipblocks/data/aggregated/kp-aggregated.zone  {6}
+                    <Russia>				https://www.ipdeny.com/ipblocks/data/aggregated/ru-aggregated.zone  {6}
+                    <Romania>				https://www.ipdeny.com/ipblocks/data/aggregated/ro-aggregated.zone  {6}
                     <Spamhaus_drop>			https://iplists.firehol.org/files/spamhaus_drop.netset  {3}
                     <Spamhaus_edrop>		https://iplists.firehol.org/files/spamhaus_edrop.netset  {3}
                     <ThreatView_OSINT>		https://threatview.io/Downloads/Experimental-IOC-Tweets.txt  {2}
                     <ThreatView_HiConf>		https://threatview.io/Downloads/IP-High-Confidence-Feed.txt  {2}
+                    <Turkey>				https://www.ipdeny.com/ipblocks/data/aggregated/tr-aggregated.zone  {6}
                     <USOM>					https://raw.githubusercontent.com/elliotwutingfeng/USOM-Blocklists/main/ips.txt  {2}"
 blocklist_ip=""
 blocklist_domain=""
