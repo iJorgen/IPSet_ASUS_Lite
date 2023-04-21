406a0ea317df782d5989ce4f7a09f1abe74cd0c0
iJorgen <52280788+iJorgen@users.noreply.github.com>
2023-04-21 10:47:07 +0200

    Update firewall.sh

--- firewall.sh
+++ firewall.sh
@@ -33,24 +33,23 @@ logmode="enabled"		# enabled | disabled
 loginvalid="enabled"	# enabled | disabled
 
 blocklist_set="		<AbuseIPDB>				https://raw.githubusercontent.com/borestad/blocklist-ip/main/abuseipdb-s100.ipv4  {1}
-					<BinaryDefense>			https://iplists.firehol.org/files/bds_atif.ipset  {2}
                     <C2IntelFeeds>			https://raw.githubusercontent.com/drb-ra/C2IntelFeeds/master/feeds/IPC2s.csv  {2}
                     <China>					https://www.ipdeny.com/ipblocks/data/aggregated/cn-aggregated.zone  {6}
 					<CIArmy>				https://iplists.firehol.org/files/ciarmy.ipset  {1}
                     <CiscoTalos>			https://www.talosintelligence.com/documents/ip-blacklist  {2}
                     <CyberCrime>			https://iplists.firehol.org/files/cybercrime.ipset  {1}
                     <DanMe_TOR>				https://iplists.firehol.org/files/dm_tor.ipset  {1}
-                    <EmergingThreats>		https://rules.emergingthreats.net/fwrules/emerging-Block-IPs.txt  {2}
+                    <EmergingThreats>		https://iplists.firehol.org/files/et_block.netset  {2}
                     <GreenSnow>				https://iplists.firehol.org/files/greensnow.ipset  {1}
                     <IPSum>					https://raw.githubusercontent.com/stamparm/ipsum/master/levels/2.txt  {2}
                     <Iran>					https://www.ipdeny.com/ipblocks/data/aggregated/ir-aggregated.zone  {6}
                     <NorthKorea>			https://www.ipdeny.com/ipblocks/data/aggregated/kp-aggregated.zone  {6}
                     <Russia>				https://www.ipdeny.com/ipblocks/data/aggregated/ru-aggregated.zone  {6}
-                    <Spamhaus_drop>			https://iplists.firehol.org/files/spamhaus_drop.netset  {3}
-                    <Spamhaus_edrop>		https://iplists.firehol.org/files/spamhaus_edrop.netset  {3}
-                    <SW_Hackers>			https://raw.githubusercontent.com/ShadowWhisperer/IPs/master/Malware/Hackers  {2}
-                    <SW_MalwareHosts>		https://raw.githubusercontent.com/ShadowWhisperer/IPs/master/Malware/Hosting  {2}
-                    <SW_Scanners>			https://raw.githubusercontent.com/ShadowWhisperer/IPs/master/Other/Scanners  {2}
+                    <Spamhaus_drop>			https://iplists.firehol.org/files/spamhaus_drop.netset  {4}
+                    <Spamhaus_edrop>		https://iplists.firehol.org/files/spamhaus_edrop.netset  {4}
+                    <SW_Hackers>			https://raw.githubusercontent.com/ShadowWhisperer/IPs/master/Malware/Hackers  {1}
+                    <SW_MalwareHosts>		https://raw.githubusercontent.com/ShadowWhisperer/IPs/master/Malware/Hosting  {1}
+                    <SW_Scanners>			https://raw.githubusercontent.com/ShadowWhisperer/IPs/master/Other/Scanners  {1}
                     <ThreatView_OSINT>		https://threatview.io/Downloads/Experimental-IOC-Tweets.txt  {2}
                     <ThreatView_HiConf>		https://threatview.io/Downloads/IP-High-Confidence-Feed.txt  {2}
                     <USOM>					https://raw.githubusercontent.com/elliotwutingfeng/USOM-Blocklists/main/ips.txt  {2}"
