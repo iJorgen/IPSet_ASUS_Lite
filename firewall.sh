a0723181ec7d0628aa690b6dc0cd106afd7c3fb7
Jörgen <52280788+iJorgen@users.noreply.github.com>
2024-06-05 20:12:13 +0200

    Update firewall.sh

--- firewall.sh
+++ firewall.sh
@@ -59,6 +59,7 @@ blocklist_set="     <AbuseIPDB>             https://raw.githubusercontent.com/bo
                     <CiscoTalos>            https://www.talosintelligence.com/documents/ip-blacklist  {11}
                     <DanMeTOR>              https://raw.githubusercontent.com/borestad/firehol-mirror/main/dm_tor.ipset  {7}
                     <EmergingThreats>       https://rules.emergingthreats.net/fwrules/emerging-Block-IPs.txt  {11}
+					<Hackers>				https://raw.githubusercontent.com/ShadowWhisperer/IPs/master/Malware/Hackers  {17}
                     <Hagezi>				https://raw.githubusercontent.com/hagezi/dns-blocklists/main/ips/tif.txt  {5}
 					<IPSum>                 https://raw.githubusercontent.com/stamparm/ipsum/master/levels/2.txt  {7}
                     <Scanners>              https://raw.githubusercontent.com/ShadowWhisperer/IPs/master/Other/Scanners  {17}"
